4026d977f3e0243ad94bb9cdd50dbef35aeea92d
r10-artem <r10-artem@monowork.pro>
2024-04-29 17:12:39 +0300

    master: update base ton connect

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const manifestUrl = "https://tg-music-app.local/tonconnect-manifest.json";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </React.StrictMode>,
-)
+  </TonConnectUIProvider>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 import fs from "fs";
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   server: {
     port: 443,
     host: "0.0.0.0",
