fd4f355db6b7b89bc4f6416077ddde2f2bccde36
r10-artem <r10-artem@monowork.pro>
2024-04-29 19:05:53 +0300

    master: fix manifest

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const manifestUrl = "https://tg-music-app.local/tonconnect-manifest.json";
+const manifestUrl = "https://ton-music.vercel.app/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
