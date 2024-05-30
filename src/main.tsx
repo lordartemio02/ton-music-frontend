034b1d17007137bb6307fe73786cdeec91b99177
r09-Andrey <r09-andrey@monowork.pro>
2024-05-31 03:12:57 +0400

    feat: Настройка компонента App

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 const manifestUrl = "https://ton-music.vercel.app/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <TonConnectUIProvider manifestUrl={manifestUrl}>
-    <App />
-  </TonConnectUIProvider>
+  <BrowserRouter>
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+      <App />
+    </TonConnectUIProvider>
+  </BrowserRouter>
 );
