0b3bb50ca3c6e3a4facbdeed1906f328ffb161be
r09-Andrey <r09-andrey@monowork.pro>
2024-05-31 03:10:06 +0400

    feat: новые иконки svg

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,17 @@
 import HeartIcon from "./HeartIcon";
-import PlayIcon from "./PlayIcon";
+import PauseIcon from "./pause.svg";
+import PlayIcon from "./play.svg";
+import PlaylistIcon from "./playlist.svg";
+import SearchIcon from "./search.svg";
+import SongIcon from "./song.svg";
+import TasksIcon from "./tasks.svg";
 
-export { HeartIcon, PlayIcon };
+export {
+  HeartIcon,
+  PauseIcon,
+  PlayIcon,
+  PlaylistIcon,
+  SearchIcon,
+  SongIcon,
+  TasksIcon,
+};
