8a7bc63cebf00d83c43f70019cd1cad545d82c31
r10-artem <r10-artem@monowork.pro>
2024-06-01 20:55:07 +0300

    master: fix modal

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,20 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { SwiperSlide } from "swiper/react";
+import {
+  BigPauseIcon,
+  BigPlayIcon,
+  BigTonIcon,
+  CloseIcon,
+} from "../../assets/icons";
 import ControlMusicPanel from "../ControlMusicPanel";
 import Header from "../Header";
+import Modal from "../Modal";
+import ModalEndContent from "../ModalEndContent";
+import ModalStartContent from "../ModalStartContent";
 import Navigation from "../Navigation";
+import Slider from "../Slider";
+import SliderPoint from "../SliderPoint";
 import { ILayout } from "./Layout.interface";
 
 const Layout: FC<ILayout> = ({ children }) => {
@@ -10,6 +22,17 @@ const Layout: FC<ILayout> = ({ children }) => {
   const rx = new RegExp(/tasks$/, "i");
 
   const isRenderMusicPanel = rx.test(location.pathname);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [activeSlide, setActiveSlide] = useState<number>(0);
+
+  useEffect(() => {
+    if (!localStorage.getItem("modal")) {
+      localStorage.setItem("modal", "true");
+      setIsOpen(true);
+    }
+  }, []);
 
   return (
     <div className="min-h-screen bg-black flex flex-col w-full px-4 pt-1">
@@ -26,6 +49,62 @@ const Layout: FC<ILayout> = ({ children }) => {
         )}
         <Navigation />
       </div>
+      <Modal handleClose={() => setIsOpen(false)} isOpen={isOpen}>
+        <div className="flex flex-col">
+          <div className="flex justify-between px-3 pt-1 mb-11">
+            <div className="w-7 h-7"></div>
+            <div className="flex flex-row items-center justify-center gap-1">
+              {[0, 1, 2].map((_, i) => (
+                <SliderPoint
+                  key={`slide-home-${i}`}
+                  isActive={i === activeSlide}
+                />
+              ))}
+            </div>
+            <button onClick={() => setIsOpen(false)}>
+              <CloseIcon />
+            </button>
+          </div>
+          <Slider onChange={(e) => setActiveSlide(e.activeIndex)}>
+            <SwiperSlide className="flex flex-col gap-11">
+              <ModalStartContent
+                footerText="button"
+                headerText="magic"
+                classNameBlur="bg-[#007AFF]"
+                icon={<BigPlayIcon />}
+              />
+              <ModalEndContent
+                headerText="Listen to music"
+                footerText="and get free crypto tokens TONM"
+              />
+            </SwiperSlide>
+            <SwiperSlide className="flex flex-col gap-11">
+              <ModalStartContent
+                footerText="music"
+                headerText="infinity"
+                classNameBlur="bg-[#B00FB4]"
+                icon={<BigPauseIcon />}
+              />
+              <ModalEndContent
+                headerText="Invite friends"
+                footerText="and get rewards"
+              />
+            </SwiperSlide>
+            <SwiperSlide className="flex flex-col gap-11">
+              <ModalStartContent
+                footerText="ton"
+                headerText="earn"
+                classNameBlur="bg-[#E53935]"
+                icon={<BigTonIcon />}
+              />
+              <ModalEndContent
+                headerText="Upload music"
+                footerText="and get royalty"
+              />
+            </SwiperSlide>
+          </Slider>
+        </div>
+      </Modal>
     </div>
   );
 };
