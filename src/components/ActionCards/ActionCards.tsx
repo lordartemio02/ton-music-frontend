b56d3b52113fcd17782900d8f219e4a543b3ed5d
r10-artem <r10-artem@monowork.pro>
2024-06-01 22:06:00 +0300

    master: update links

--- src/components/ActionCards/ActionCards.tsx
+++ src/components/ActionCards/ActionCards.tsx
@@ -1,10 +1,7 @@
 import { FC } from "react";
-import { useNavigate } from "react-router-dom";
 import { actionCardsList } from "./ActionCards.config";
 
 const ActionCards: FC = () => {
-  const navigate = useNavigate();
-
   return (
     <div className="grid grid-cols-2 gap-[9px]">
       {actionCardsList.map((item, index) => (
