f1b25683494d3939e7fe52fe3420a65abbf7682b
r10-artem <r10-artem@monowork.pro>
2024-06-09 01:42:08 +0300

    master: update haptic

--- src/components/CrazyFrog/CrazyFrog.tsx
+++ src/components/CrazyFrog/CrazyFrog.tsx
@@ -3,12 +3,14 @@ import { FC, useEffect, useRef, useState } from "react";
 import { CrazyFrogIcon } from "../../assets/icons";
 import "./style.css";
 
+import { useHapticFeedback } from "@tma.js/sdk-react";
 import { useAudioPlayer, useGlobalAudioPlayer } from "react-use-audio-player";
 import data from "../../mock/audiolist.json";
 
 const CrazyFrog: FC = () => {
   const [scale, setScale] = useState(1);
   const ref = useRef<HTMLDivElement>(null);
+  const { impactOccurred } = useHapticFeedback();
   const [error, setError] = useState<any>({});
 
   const outerRef = useRef(null);
@@ -102,7 +104,7 @@ const CrazyFrog: FC = () => {
         window.navigator.vibrate(5);
       } catch (error) {
         setError(error);
-        // impactOccurred("light");
+        impactOccurred("light");
       }
 
       setBlockPositions((prev: any) => [...prev, { x: offsetX, y: offsetY }]);
